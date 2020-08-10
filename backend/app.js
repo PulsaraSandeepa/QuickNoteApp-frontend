aff0cf9c20d2a901edbd2d51b599190e177b1d15
Pulsara Sandeepa <pulsarasandeepa@gmail.com>
2020-08-11 01:14:54 +0530

    initialized the db

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
+const app = express();
+
+mongoose.connect("mongodb+srv://pulsara:postapp@cluster0.teni8.mongodb.net/post-app?retryWrites=true&w=majority\n")
+  .then(() => {
+    console.log('Connected to database');
+  }).catch(() => {
+    console.log('Connection Error');
+
+})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type,Accept");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS");
+  next();
+});
+
+
+app.post('/api/posts', (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  })
+
+  console.log(post);
+  post.save();
+  res.status(201).json({
+    message: 'Post added Successfully'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
+  //console.log('First middleware');
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched successfully',
+      posts: documents
+    });
+  });
+});
+
+module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {MatCardModule} from '@angular/material/card';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
+import {AppComponent} from './app.component';
+import {PostCreateComponent} from './posts/post-create/post-create.component';
 import {MatButtonModule} from '@angular/material/button';
-import { HeaderComponent } from './header/header.component';
+import {HeaderComponent} from './header/header.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { PostListComponent } from './post-list/post-list.component';
+import {PostListComponent} from './post-list/post-list.component';
 import {MatExpansionModule} from '@angular/material/expansion';
 
 
@@ -29,9 +30,11 @@ import {MatExpansionModule} from '@angular/material/expansion';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
